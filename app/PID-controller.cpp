561bfcad5f3790aaa993df51df02c40ad9281651
Eashwar Sathyamurthy <eashwar.sathyamurthy@gmail.com>
2019-09-28 14:43:26 -0400

    test

--- app/PID-controller.cpp
+++ app/PID-controller.cpp
@@ -5,9 +5,6 @@
 #include "lib.h"
 
 double PidController::compute(double actual, double set) {
-<<<<<<< HEAD
-    return 99;
-=======
     return newVelocity;
->>>>>>> 5fda9da5953fdc10d4aa759cb63489fbbaeaca70
+ 
 }
