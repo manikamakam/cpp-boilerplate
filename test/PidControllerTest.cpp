e64c1381f1270faa819ce35d15919cef7d2735b9
manikamakam <srimanika1998@gmail.com>
2019-09-27 18:11:53 -0700

    test

--- test/PidControllerTest.cpp
+++ test/PidControllerTest.cpp
@@ -3,9 +3,7 @@
 #include <iostream>
 #include "lib.h"
 
-TEST(compute, justForFun) {
-    //std::shared_ptr<PidController> velocityFinal = std::make_shared_ptr<PidController>(); 
-    //std::unique_ptr<PidController> velocityFinal (new PidController);
-    PidController velocityFinal;
-    EXPECT_EQ(17.0, compute(8.0,9.0));
+TEST(PidControllerTest, shouldPass) {
+    std::shared_ptr<PidController> velocityFinal (new PidController); 
+    ASSERT_NEAR(9.0, velocityFinal-> compute(8.0,9.0),1.0);
 }
