a60400091a73f037646ede6b0f9223355fe61969
manikamakam <srimanika1998@gmail.com>
2019-09-27 18:35:28 -0700

    2 tests done

--- test/PidControllerTest.cpp
+++ test/PidControllerTest.cpp
@@ -6,4 +6,12 @@
 TEST(PidControllerTest, shouldPass) {
     std::shared_ptr<PidController> velocityFinal (new PidController); 
     ASSERT_NEAR(9.0, velocityFinal-> compute(8.0,9.0),1.0);
+    ASSERT_NEAR(9.0, velocityFinal-> compute(-6.0,9.0),1.0);
 }
+
+
+TEST(PidControllerTest, shouldPass1) {
+    std::shared_ptr<PidController> velocityFinal (new PidController); 
+    ASSERT_NEAR(10.0, velocityFinal-> compute(-6.0,10.0),0.5);
+}
+
