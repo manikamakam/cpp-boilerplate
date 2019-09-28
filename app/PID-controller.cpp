63aff0108adafb3161cc8459890bff92a2929c4b
Eashwar Sathyamurthy <eashwar.sathyamurthy@gmail.com>
2019-09-28 17:24:56 -0400

    Changes

--- app/PID-controller.cpp
+++ app/PID-controller.cpp
@@ -1,10 +1,15 @@
+/** Copyright 2019 <Eashwar> <Sri Manika> 
+ */
+
 /** @file PID-controller.cpp
- * @brief This file provides the implementation of the compute method 	
+ * @brief This file provides the implementation of the compute method
+ * @author Part 1: Sri Manika(Driver) Eashwar(Navigator)
  */
 
 #include "lib.h"
 
 double PidController::compute(double actual, double set) {
+   /// Take kp=0.01, ki=0.2, kd=0.1
     return newVelocity;
- 
 }
+
