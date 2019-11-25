312fe911afd9e82982344fe5fe8ca9bbaac29e65
AbhishekB <abhibanerjee2494@gmail.com>
2019-11-25 12:27:45 -0500

    [tets][main.cpp] Added license and doxygen comments

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,43 @@
-/** Copyright 2019 <Eashwar Sathyamurthy> <Sri Manika Makam> 
+/**
+ *  @file      main.cpp
+ *  @author    Sri Manika Makam
+ *  @copyright MIT License
+ *  @brief     Main function to run the tests
  */
 
-/** @file main.cpp
- *  @brief This file will run all test cases.
- *  @author Part 1: Sri Manika Makam(Driver) Eashwar Sathyamurthy(Navigator)
- *  @author Part 2: Andre Ferreira(Driver) Nakul Patel(Navigator)
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Sri Manika Makam
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
  */
 
 #include <gtest/gtest.h>
-#include <PidController.h>
 
+/**
+ * @brief      main function
+ * @param      argc of type int
+ * @param      argv of type char
+ * @return     int of value zero
+ */
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
